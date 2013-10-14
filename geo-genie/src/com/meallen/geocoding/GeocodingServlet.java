df1ca4cf8c40c0c45c3e82b3f399acf0d37940f0
mattheweallen <mattheweallen@gmail.com>
2013-10-14 17:27:42 -0500

    first commit

---
+++ geo-genie/src/com/meallen/geocoding/GeocodingServlet.java
@@ -0,0 +1,41 @@
+package com.meallen.geocoding;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GeocodingServlet
+ */
+@WebServlet(description = "This is a Geocoding Servlet", urlPatterns = { "/latlon" })
+public class GeocodingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GeocodingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("In Get");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("In Post");
+	}
+
+}
