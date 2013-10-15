d834ac643e4dca3f0bab5b853aac640445744916
mattheweallen <mattheweallen@gmail.com>
2013-10-15 06:04:26 -0500

    Getting response back from Google JSON

--- geo-genie/src/com/meallen/geocoding/GeocodingServlet.java
+++ geo-genie/src/com/meallen/geocoding/GeocodingServlet.java
@@ -2,6 +2,10 @@ package com.meallen.geocoding;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
@@ -9,6 +13,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 /**
  * Servlet implementation class GeocodingServlet
@@ -55,6 +68,22 @@ public class GeocodingServlet extends HttpServlet {
 //		
 //		System.out.println("}");
 //		System.out.println("]");
+		
+		Map<String, String> parms = new HashMap<String, String>();
+		parms.put("latlng", lat + "," + lon);
+		parms.put("sensor","false");
+		
+		URI uri;
+		String entity;
+		try {
+			uri = buildURL("http","maps.googleapis.com", "/maps/api/geocode/json", parms);
+			entity = getResponseBody(uri);
+			System.out.println(entity);
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		try {
 			System.out.println("[{\"id\":\"d1b3eb46-3833-451f-b927-592de14cce18\",\"query\":{\"lat\":\"43.81\",\"lon\":\"-91.23\"},\"response\":{\"address\":\"1702-1720 King Street, La Crosse, WI 54601, USA\",\"sourceIp\":\"138.49.196.103\",\"temp\":\"60.80\",\"time\":1381794724959,\"weather\":\"Sky is Clear\"}}]");
 			out.println("[{\"id\":\"d1b3eb46-3833-451f-b927-592de14cce18\",\"query\":{\"lat\":\"43.81\",\"lon\":\"-91.23\"},\"response\":{\"address\":\"1702-1720 King Street, La Crosse, WI 54601, USA\",\"sourceIp\":\"138.49.196.103\",\"temp\":\"60.80\",\"time\":1381794724959,\"weather\":\"Sky is Clear\"}}]");
@@ -64,6 +93,31 @@ public class GeocodingServlet extends HttpServlet {
 			out.close();
 		}
 	}
+	
+	private String getResponseBody(URI uri) throws ClientProtocolException, IOException {
+		String responseBody;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(uri.toString());
+		CloseableHttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		responseBody = EntityUtils.toString(entity);
+		//maybe surround with try catch and put response.close in finally
+		
+		response.close();
+		return responseBody;
+	}
+	
+	private URI buildURL(String scheme, String host, String path, Map<String, String> parms) throws URISyntaxException {
+		URIBuilder uriBuilder = new URIBuilder()
+					.setScheme(scheme)
+					.setHost(host)
+					.setPath(path);
+		for(String key : parms.keySet()) {
+			uriBuilder.setParameter(key, parms.get(key));
+		}
+		return uriBuilder.build();
+	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
