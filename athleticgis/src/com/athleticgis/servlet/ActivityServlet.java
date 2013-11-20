5cb15d14c71d428e07bfd925c550e2822fde991a
mattheweallen <mattheweallen@gmail.com>
2013-11-19 18:45:09 -0600

    Added Activity Servlet

---
+++ athleticgis/src/com/athleticgis/servlet/ActivityServlet.java
@@ -0,0 +1,45 @@
+package com.athleticgis.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ActivityServlet
+ */
+@WebServlet("/ActivityServlet")
+public class ActivityServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActivityServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String idStr = request.getParameter("activityId");
+		System.out.println(idStr);
+		out.println("{\"coordinates\": [{ \"lat\":43.810909 , \"lng\":-91.256091 }, { \"lat\":43.822571 , \"lng\":-91.243666 }, { \"lat\":43.812151 , \"lng\":-91.257099 }]}");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
