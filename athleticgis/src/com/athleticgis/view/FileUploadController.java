f6e2603ef0a93688edc9b1e92fc22d4285d26102
mattheweallen <mattheweallen@gmail.com>
2013-11-25 16:44:12 -0600

    move to prime faces. file upload working.

---
+++ athleticgis/src/com/athleticgis/view/FileUploadController.java
@@ -0,0 +1,92 @@
+package com.athleticgis.view;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;  
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;  
+  
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.alternativevision.gpx.GPXParser;
+import org.alternativevision.gpx.beans.GPX;
+import org.alternativevision.gpx.beans.Track;
+import org.alternativevision.gpx.beans.Waypoint;
+import org.primefaces.event.FileUploadEvent;  
+import org.primefaces.model.UploadedFile;  
+import org.xml.sax.SAXException;
+
+import com.athleticgis.UserInfoBean;
+import com.athleticgis.model.AthleticgisFacade;
+import com.athleticgis.model.gis.ActivityPoint;
+
+@ManagedBean
+@RequestScoped
+public class FileUploadController { 
+	@ManagedProperty(value = "#{userInfoBean}")
+    private UserInfoBean userInfoBean;
+  
+    /**
+	 * @return the userInfoBean
+	 */
+	public UserInfoBean getUserInfoBean() {
+		return userInfoBean;
+	}
+
+	/**
+	 * @param userInfoBean the userInfoBean to set
+	 */
+	public void setUserInfoBean(UserInfoBean userInfoBean) {
+		this.userInfoBean = userInfoBean;
+	}
+
+	public void handleFileUpload(FileUploadEvent event) throws ParserConfigurationException, SAXException {  
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+        upload(event.getFile());
+    } 
+    
+public void upload(UploadedFile file) throws ParserConfigurationException, SAXException {
+		
+        try {
+            
+            InputStream in=file.getInputstream();
+            
+            GPXParser p = new GPXParser();
+            GPX gpx = p.parseGPX(in);
+            com.athleticgis.model.gis.Activity a = new com.athleticgis.model.gis.Activity();
+            a.setName(file.getFileName());
+            //AthleticgisFacade af = new AthleticgisFacade();
+            a.setUser(AthleticgisFacade.findUserByUsername(userInfoBean.getName()));
+            
+            List<ActivityPoint> activityPoints = new ArrayList<ActivityPoint>();
+            for(Track t : gpx.getTracks()) {
+            	for(Waypoint  wp : t.getTrackPoints()) {
+            		//System.out.println(wp.getLatitude() + "," + wp.getLongitude());
+            		com.athleticgis.model.gis.ActivityPoint activityPoint = new ActivityPoint();
+            		activityPoint.setLatitude(wp.getLatitude());
+            		activityPoint.setLongitude(wp.getLongitude());
+            		activityPoint.setTime(new Timestamp(wp.getTime().getTime()));
+            		activityPoint.setElevation(wp.getElevation());
+            		activityPoints.add(activityPoint);
+            	}
+            }
+            a.setWaypoints(activityPoints);
+            
+            
+            AthleticgisFacade.persistActivityAndActivityPoints(a, activityPoints);
+            
+            in.close();
+            //br.close();
+        } catch (IOException ex) {
+           ex.printStackTrace();
+        }
+	}
+} 
