0fd16c0311a26ede3ebde770f36cc1f3156c74e0
mattheweallen <mattheweallen@gmail.com>
2013-11-29 14:53:45 -0600

    GIS Calculator

--- athleticgis/src/com/athleticgis/util/gis/GISCalculator.java
+++ athleticgis/src/com/athleticgis/util/gis/GISCalculator.java
@@ -51,4 +51,31 @@ public class GISCalculator {
 	private double deg2rad(double deg) {
 		return (deg * Math.PI / 180.0);
 	}
+	
+	public void centerMap() {
+//		int minLat = Integer.MAX_VALUE;
+//		int maxLat = Integer.MIN_VALUE;
+//		int minLon = Integer.MAX_VALUE;
+//		int maxLon = Integer.MIN_VALUE;
+
+//		for (GeoPoint item : items) 
+//		{ 
+//
+//		      int lat = item.getLatitudeE6();
+//		      int lon = item.getLongitudeE6();
+//
+//		      maxLat = Math.max(lat, maxLat);
+//		      minLat = Math.min(lat, minLat);
+//		      maxLon = Math.max(lon, maxLon);
+//		      minLon = Math.min(lon, minLon);
+//		 }
+
+//		mapController.zoomToSpan(Math.abs(maxLat - minLat), Math.abs(maxLon - minLon));
+//		mapController.animateTo(new GeoPoint( (maxLat + minLat)/2, 
+//		(maxLon + minLon)/2 )); 
+//		
+//		
+//		double fitFactor = 1.5;
+//		mapController.zoomToSpan((int) (Math.abs(maxLat - minLat) * fitFactor), (int)(Math.abs(maxLon - minLon) * fitFactor));
+	}
 }
