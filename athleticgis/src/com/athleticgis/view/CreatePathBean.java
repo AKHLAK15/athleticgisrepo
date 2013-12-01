6c1a0c7b013d3b29e6c94c398cb529451b75f6c9
mattheweallen <mattheweallen@gmail.com>
2013-12-01 11:56:19 -0600

    save my map method

--- athleticgis/src/com/athleticgis/view/CreatePathBean.java
+++ athleticgis/src/com/athleticgis/view/CreatePathBean.java
@@ -1,14 +1,27 @@
 package com.athleticgis.view;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;  
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;  
 import javax.faces.event.ActionEvent;  
   
+
+
+
+
+
+
+
+
 
 
 
@@ -20,10 +33,15 @@ import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
 import org.primefaces.model.map.Polyline;
 
+import com.athleticgis.domain.activity.MyMap;
+import com.athleticgis.domain.activity.MyMapMarker;
+import com.athleticgis.model.AthleticgisFacade;
+
 @ManagedBean
 @ViewScoped
 public class CreatePathBean implements Serializable {
 	private static final long serialVersionUID = -8907242457718889989L;
+	private List<MyMapMarker> myMapMarkers = new ArrayList<MyMapMarker>();
 //	private MapModel emptyModel = new DefaultMapModel();
 //	Polyline polyline;
 //	
@@ -32,9 +50,43 @@ public class CreatePathBean implements Serializable {
       
     private double lat;  
       
-    private double lng;  
+    private double lng; 
+    private String myMapName;
+    
+
+
+	/**
+	 * @return the myMapName
+	 */
+	public String getMyMapName() {
+		return myMapName;
+	}
+
+	/**
+	 * @param myMapName the myMapName to set
+	 */
+	public void setMyMapName(String myMapName) {
+		this.myMapName = myMapName;
+	}
+
+	@ManagedProperty(value = "#{userInfoBean}")
+	private UserInfoBean userInfoBean;
   
-    public CreatePathBean() {  
+    /**
+	 * @return the userInfoBean
+	 */
+	public UserInfoBean getUserInfoBean() {
+		return userInfoBean;
+	}
+
+	/**
+	 * @param userInfoBean the userInfoBean to set
+	 */
+	public void setUserInfoBean(UserInfoBean userInfoBean) {
+		this.userInfoBean = userInfoBean;
+	}
+
+	public CreatePathBean() {  
 //    	//emptyModel = new DefaultMapModel(); 
 //    	polyline = new Polyline();
 //    	//are these necessary?
@@ -82,8 +134,13 @@ public class CreatePathBean implements Serializable {
     	
     	System.out.println("title: " + title + "," + "lat: " + lat + "," + "lng: " + lng);
     	//polyline.getPaths().add(coord);
-          
-        addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Point Added", "Lat:" + lat + ", Lng:" + lng));  
+        MyMapMarker m = new MyMapMarker();  
+        m.setLatitude(lat);
+        m.setLongitude(lng);
+        m.setName(title);
+        m.setTime(new Timestamp(new Date().getTime()));
+        myMapMarkers.add(m);
+        //addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Point Added", "Lat:" + lat + ", Lng:" + lng));  
     }
     
     public String getCenter() {
@@ -92,10 +149,14 @@ public class CreatePathBean implements Serializable {
 	}
     
     public String savePath() {
-    	return null;
+    	MyMap myMap = new MyMap();
+    	myMap.setUser(AthleticgisFacade.findUserByUsername(userInfoBean.getUsername()));
+    	myMap.setName(myMapName);
+    	return "createpath";
     }
     
     public String restartPath() {
-    	return null;
+    	myMapMarkers = new ArrayList<MyMapMarker>();
+    	return "createpath";
     }
 }
